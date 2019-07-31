5137212f05c7982de9a6d5e08dcc4fbef44d4023
Ricky Roy <rickyproy@gmail.com>
2019-07-31 12:34:03 -0700

    adding stock

---
+++ stock.ps1
@@ -0,0 +1,75 @@
+
+
+#While(1) {
+For-Each ($filename in Get-ChildItem | Where-Object {".mp4",".mov", "avi" -eq $_.extension}) {
+  $filename = Read-Host -Prompt 'What is the filename? (no extension)'
+  $exists = Test-Path $filename
+  If($exists) {
+  } Else {
+
+    $filename = $filename + ".mp4"
+    if(Test-Path $filename) { }
+    Else {
+      Invoke-Expression 'clear'
+      Invoke-Expression 'ls'
+      Write-Host ''
+      Write-Warning 'Invalid filename try again'
+      Write-Host ''
+      continue
+    }
+
+  }
+  $trim = Read-Host -Prompt 'Trim the Video Length? (y/n)'
+  $trim_code = ''
+
+  $fullPath = 'C:\Script_Outputs\edited_videos\'
+
+  If(!(test-path $fullPath))
+  {
+        New-Item -ItemType Directory -Force -Path $fullPath
+  }
+
+  If($trim -eq 'y') {
+    $begin = Read-Host -Prompt 'What is the start timestamp?'
+    $duration = Read-Host -Prompt 'What is the duration?'
+    $trim_code = (' -ss ' + $begin + ' -t ' + $duration)
+  }
+
+  $audio_code = '-an'
+  $video_code = ' -vcodec copy '
+
+  $edit_video = Read-Host -Prompt 'Edit Video Gamma/Saturation?'
+  $video_satisfied = 'n'
+  If($edit_video -eq 'y') {
+    $gamma = 1.0
+    $saturation = 1.0
+    $contrast = 1.0
+    While($video_satisfied -ne 'y') {
+      If($video_satisfied -eq 'q') {
+        break
+      }
+      $gamma = Read-Host -Prompt 'Gamma (0.1 - 10.0) (1.0 = normal)?'
+      $saturation = Read-Host -Prompt 'Saturation (0.0 - 3.0) (1.0 = normal)?'
+      $contrast = Read-Host -Prompt 'Contrast (-2.0 - 2.0) (1.0 = normal)?'
+      $shouldPreview = Read-Host -Prompt 'Preview Video? (y/n)'
+      If($shouldPreview -eq 'y') {
+        $previewString = 'ffplay -vf "eq=gamma=' + $gamma + ':saturation=' + $saturation + ':contrast=' + $contrast + '" ' + '"' + $filename + '"'
+        Write-Host $previewString
+        Invoke-Expression $previewString
+        $video_satisfied = Read-Host -Prompt 'Keep Video Settings? (y/n)'
+      }
+    }
+    If($video_satisfied -eq 'y') {
+      $video_code = ' -vf "eq=gamma=' + $gamma + ':saturation=' + $saturation + ':contrast=' + $contrast + '" '
+    }
+  }
+  #ffmpeg -i $filename -ss $begin -t $duration -vcodec copy $audio_code copy G:\Stock_footage\$filename
+  #ffmpeg -i $filename $trim_code -vcodec copy $audio_code copy G:\Stock_footage\$filename
+
+  $finalString = 'ffmpeg -i "' + $filename + '"' + $trim_code + $video_code + $audio_code + ' "' + $fullPath + $filename + '"'
+  Write-Host $finalString
+  Invoke-Expression $finalString
+  Write-Host ""
+  Write-Warning "Success"
+  Write-Host ""
+}
