54b2e28abc2ff533267b6b3d4678162f45ecad92
Ricky <ricky@Rickys-MacBook-Pro.local>
2020-04-02 01:08:25 -0700

    updated watermark logic and added png

--- createSampleVideosWithWatermark.sh
+++ createSampleVideosWithWatermark.sh
@@ -1,5 +1,5 @@
 # Aliases
 # Establishing custom commands below
 
-for i in *.mp4 *.m4v *.mov; do ffmpeg -i "$i" -i /Users/ricky/Movies/renders/watermark/watermark.png \
+for i in *.mp4 *.m4v *.mov; do ffmpeg -i "$i" -i /Users/ricky/Code/Scripts/watermark.png \
 -filter_complex "[0:v]scale=1280:-2[bg];[bg][1:v]overlay=0:0" -c:v libx264 "/Users/ricky/Movies/renders/samples/${i%.*}_sample.mp4"; done
