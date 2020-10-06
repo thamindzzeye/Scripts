168bfdc7784b243448da4e212bc8f1048407d1c4
Ricky Roy <rickyroy@Leviathan.local>
2020-10-05 21:53:41 -0700

    added new mac scripts

---
+++ youtube.py
@@ -0,0 +1,25 @@
+
+import os, subprocess, platform
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
+
+destination = '~/Downloads/YoutubeVids/'
+# fileLocation = '~/Downloads/YoutubeVids/(title)s.%(ext)s'
+fileLocation = '\'~/Downloads/YoutubeVids/%(title)s.%(ext)s\''
+if not os.path.exists(destination):
+    os.makedirs(destination)
+
+
+url = input('What is the youtube (share) URL? :  ')
+
+command = 'youtube-dl -f \'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio\' --merge-output-format mp4 --output ' + fileLocation + ' ' + '\'' + url + '\''
+print(command)
+os.system(command)
+
+print("""
+
+Complete
+
+""")
