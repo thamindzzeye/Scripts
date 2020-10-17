3442db6cf4fbccf1321ed05b2f392f29bf4e55f2
Ricky Roy <rickyroy@Leviathan.local>
2020-10-16 18:47:44 -0700

    added alpha for img2mov

--- createVideoFromImages.py
+++ createVideoFromImages.py
@@ -24,15 +24,24 @@ and include the file extension too ".png" or ".jpg" etc
 
 
 """)
-fileInput = input('What is the file name structure of the image? ')
-framerate = input('What framerate do you want for the movie? ')
-outputName = input('What Should the Video be named (no extension)? ')
+fileInput = input('What is the file name structure of the image? : ')
+framerate = input('What framerate do you want for the movie? : ')
+outputName = input('What Should the Video be named (no extension)? : ')
+print("""
+Do you want the alpha channel in the video?
+The background will be invisible, but the file size will be MUCH larger
+
+""")
+isAlpha = input('Use Alpha Channel? (y)es / (n)o) : ')
 workingDir = os.getcwd()
 
 fileLocation = '~/Downloads/' + outputName + '.mp4'
 
-#ffmpeg -framerate 24 -i $fileInput -c:v libx264 -pix_fmt yuv420p $fullPath
-command = 'ffmpeg -framerate ' + framerate + ' -i \'' + fileInput + '\' -c:v libx264 -pix_fmt yuv420p ' + fileLocation
+if (isAlpha == 'y' or isAlpha == 'Y'):
+    command = 'ffmpeg -framerate ' + framerate + ' -i \'' + fileInput + '\' -codec prores_ks -pix_fmt yuva444p10le -alpha_bits 16 -profile:v 4444 -f mov ' + fileLocation
+    #ffmpeg -framerate $framerate -i $fileInput -codec prores_ks -pix_fmt yuva444p10le -alpha_bits 16 -profile:v 4444 -f mov $fullPath
+else:
+    command = 'ffmpeg -framerate ' + framerate + ' -i \'' + fileInput + '\' -c:v libx264 -pix_fmt yuv420p ' + fileLocation
 print(command)
 os.system(command)
 
